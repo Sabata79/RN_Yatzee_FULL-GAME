6087b97e1ceb9b5362763104ebda58033d2c0b86
Sabata 79 <sabata79@hotmail.com>
2024-12-06 12:34:09 +0200

    Added function to view other players Playercard

--- components/Scoreboard.js
+++ components/Scoreboard.js
@@ -1,19 +1,21 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, ScrollView, ImageBackground, Modal, TouchableOpacity } from 'react-native';
+import { View, Text, ScrollView, ImageBackground, TouchableOpacity, Modal } from 'react-native';
 import { DataTable } from 'react-native-paper';
 import { FontAwesome5 } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import styles from '../styles/styles';
 import { NBR_OF_SCOREBOARD_ROWS } from '../constants/Game';
 import { database } from './Firebase';
 import { ref, onValue } from 'firebase/database';
 import * as SecureStore from 'expo-secure-store';
+import PlayerCard from './PlayerCard'; // Import PlayerCard modal
 
 export default function Scoreboard({ navigation }) {
-  const [scores, setScores] = useState([]); // State, joka tallentaa tulokset
-  const [scoreType, setScoreType] = useState('allTime'); // Oletus on 'allTime'
-  const [userId, setUserId] = useState(''); 
+  const [scores, setScores] = useState([]);
+  const [scoreType, setScoreType] = useState('allTime'); // Default is 'allTime'
+  const [userId, setUserId] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-  const [cachedScores, setCachedScores] = useState(null); 
+  const [selectedPlayer, setSelectedPlayer] = useState(null); // Tila pelaajalle, jonka tiedot näytetään modaaliin
 
   useEffect(() => {
     SecureStore.getItemAsync('user_id').then((storedUserId) => {
@@ -22,85 +24,72 @@ export default function Scoreboard({ navigation }) {
       }
     });
 
-    const unsubscribe = navigation.addListener('focus', () => {
-      getScoreboardData(); 
-    });
+    getScoreboardData();
+  }, [scoreType]);
 
-    getScoreboardData(); 
-    
-    return unsubscribe;
-  }, [navigation, scoreType]);
+  const getScoreboardData = () => {
+    const playersRef = ref(database, 'players');
+    onValue(playersRef, snapshot => {
+      const playersData = snapshot.val();
+      const tmpScores = [];
+      const currentMonth = new Date().getMonth();
+      const currentYear = new Date().getFullYear();
 
-const getScoreboardData = () => {
-  const playersRef = ref(database, 'players');
-  onValue(playersRef, snapshot => {
-    const playersData = snapshot.val();
-    const tmpScores = [];
-    const currentMonth = new Date().getMonth(); 
-    const currentYear = new Date().getFullYear(); 
-
-    if (playersData) {
-      Object.keys(playersData).forEach(playerId => {
-        if (playersData[playerId] && playersData[playerId].name) {
+      if (playersData) {
+        Object.keys(playersData).forEach(playerId => {
           const player = playersData[playerId];
 
-          let scoresToUse = [];
-          if (scoreType === 'monthly') {
-            scoresToUse = Object.values(player.scores || {}).filter(score => {
-
-              // Tarkistetaan, että 'score.date' on kelvollinen
-              const dateParts = score.date.split('.'); 
-              if (dateParts.length === 3) {
-                // Muodostetaan uusi päivämäärä muodossa YYYY-MM-DD
-                const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
-                const scoreDate = new Date(formattedDate);
-
-                if (isNaN(scoreDate)) {
-                  return false; 
+          if (player.scores) {
+            let scoresToUse = [];
+            if (scoreType === 'monthly') {
+              // Suodatin kuukauden mukaan
+              scoresToUse = Object.values(player.scores).filter(score => {
+                const dateParts = score.date.split('.');
+                if (dateParts.length === 3) {
+                  const formattedDate = `${dateParts[2]}-${dateParts[1]}-${dateParts[0]}`;
+                  const scoreDate = new Date(formattedDate);
+                  if (isNaN(scoreDate)) return false;
+                  return scoreDate.getMonth() === currentMonth && scoreDate.getFullYear() === currentYear;
                 }
-
-                return scoreDate.getMonth() === currentMonth && scoreDate.getFullYear() === currentYear;
-              } else {
                 return false;
-              }
-            });
-          } else if (scoreType === 'allTime') {
-            scoresToUse = Object.values(player.scores || []);
-          }
-
-          if (scoresToUse.length > 0) {
-            let bestScore = null;
-            scoresToUse.forEach(score => {
-              if (!bestScore || score.points > bestScore.points) {
-                bestScore = score;
-              }
-            });
-
-            if (bestScore) {
-              tmpScores.push({
-                ...bestScore,
-                name: player.name,
-                playerId: playerId,
               });
+            } else if (scoreType === 'allTime') {
+              scoresToUse = Object.values(player.scores);
+            }
+
+            if (scoresToUse.length > 0) {
+              let bestScore = null;
+              scoresToUse.forEach(score => {
+                if (!bestScore || score.points > bestScore.points) {
+                  bestScore = score;
+                }
+              });
+
+              if (bestScore) {
+                tmpScores.push({
+                  ...bestScore,
+                  name: player.name,
+                  playerId: playerId,
+                  scores: Object.values(player.scores)
+                });
+              }
             }
           }
-        }
-      });
+        });
 
-      // Lajitellaan tulokset
-      const sortedScores = tmpScores.sort((a, b) => b.points - a.points);
+        const sortedScores = tmpScores.sort((a, b) => b.points - a.points);
+        setScores(sortedScores);
+      }
+    });
+  };
 
-      setScores(sortedScores); 
-    } else {
-      setScores([]);
-    }
-  });
-};
+  const handlePlayerCard = (playerId, playerName, playerScores) => {
+    setSelectedPlayer({ playerId, playerName, playerScores });
+    setModalVisible(true);
+  };
 
   return (
-    <ImageBackground
-      source={require('../assets/diceBackground.jpg')}
-      style={styles.background}>
+    <ImageBackground source={require('../assets/diceBackground.jpg')} style={styles.background}>
       <View style={styles.overlay}>
         <ScrollView style={styles.container}>
           <View style={styles.scoresHeaderContainer}>
@@ -126,84 +115,45 @@ const getScoreboardData = () => {
           ) : (
             <DataTable style={styles.scoreboardContainer}>
               <DataTable.Header>
-                <DataTable.Title style={styles.cell}>
-                  <Text style={styles.scoreboardHeader}>Rank #</Text>
-                </DataTable.Title>
-                <DataTable.Title style={styles.cell}>
-                  <Text style={styles.scoreboardHeader}>Name</Text>
-                </DataTable.Title>
-                <DataTable.Title style={styles.cell}>
-                  <Text style={styles.scoreboardHeader}>Date</Text>
-                </DataTable.Title>
-                <DataTable.Title style={styles.cell}>
-                  <Text style={styles.scoreboardHeader}>Duration</Text>
-                </DataTable.Title>
-                <DataTable.Title style={styles.cell}>
-                  <Text style={styles.scoreboardHeader}>Points</Text>
-                </DataTable.Title>
+                <DataTable.Title style={styles.cell}><Text style={styles.scoreboardHeader}>Rank #</Text></DataTable.Title>
+                <DataTable.Title style={styles.cell}><Text style={styles.scoreboardHeader}>Name</Text></DataTable.Title>
+                <DataTable.Title style={styles.cell}><Text style={styles.scoreboardHeader}>Points</Text></DataTable.Title>
               </DataTable.Header>
 
               {scores.slice(0, NBR_OF_SCOREBOARD_ROWS).map((score, index) => (
-                <DataTable.Row
-                  key={score.key}
-                  style={score.playerId === userId ? { borderWidth: 0.6, borderColor: 'red', backgroundColor: '#ffffff82' } : {}}>
+                <DataTable.Row key={score.playerId} onPress={() => handlePlayerCard(score.playerId, score.name, score.scores)}>
                   <DataTable.Cell style={styles.cell}>
                     {index === 0 && <FontAwesome5 name="medal" size={30} color="gold" />}
                     {index === 1 && <FontAwesome5 name="medal" size={25} color="silver" />}
                     {index === 2 && <FontAwesome5 name="medal" size={20} color="brown" />}
-                    {index > 2 && <Text style={styles.scoreboardText}>{index + 1}.</Text>}
+                    {index > 2 && <Text style={styles.scoreboardText}>{index + 1}</Text>}
                   </DataTable.Cell>
-                  <DataTable.Cell style={styles.cell}>
-                    <Text style={styles.scoreboardText}>{score.name}</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell style={styles.cell}>
-                    <Text style={[styles.scoreboardText, { fontSize: 10 }]}>{score.date}</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell style={styles.cell}>
-                    <Text style={styles.scoreboardText}>{score.duration}s</Text>
-                  </DataTable.Cell>
-                  <DataTable.Cell style={styles.cell}>
-                    <Text style={[styles.scoreboardText, { fontSize: 14 }]}>{score.points}</Text>
-                  </DataTable.Cell>
+                  <DataTable.Cell style={styles.cell}><Text style={styles.scoreboardText}>{score.name}</Text></DataTable.Cell>
+                  <DataTable.Cell style={styles.cell}><Text style={styles.scoreboardText}>{score.points}</Text></DataTable.Cell>
                 </DataTable.Row>
               ))}
             </DataTable>
           )}
         </ScrollView>
-
-        <TouchableOpacity
-          style={styles.infoButton}
-          onPress={() => setModalVisible(true)}>
-          <FontAwesome5 name="info-circle" size={40} color="white" />
-        </TouchableOpacity>
-
-        <Modal
-          animationType="slide"
-          transparent={true}
-          visible={modalVisible}
-          onRequestClose={() => setModalVisible(false)}>
-          <View style={styles.modalCenteredView}>
-            <View style={styles.modalView}>
-              <TouchableOpacity
-                style={styles.modalCloseButton}
-                onPress={() => setModalVisible(false)}>
-                <Text style={styles.modalCloseButtonText}>X</Text>
-              </TouchableOpacity>
-
-              <Text style={styles.modalText}>How Scores Are Compared</Text>
-              <Text style={styles.modalSubText}>
-                1. **Points**: Higher points are ranked first.
-              </Text>
-              <Text style={styles.modalSubText}>
-                2. **Duration**: If points are equal, the score with the shorter duration comes first.
-              </Text>
-              <Text style={styles.modalSubText}>
-                3. **Date/Time**: If both points and duration are equal, the score that was achieved earlier is ranked higher.
-              </Text>
-            </View>
-          </View>
-        </Modal>
       </View>
+
+      {/* PlayerCard modal for displaying selected player's details */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        {selectedPlayer && (
+          <PlayerCard
+            playerId={selectedPlayer.playerId}
+            playerName={selectedPlayer.playerName}
+            playerScores={selectedPlayer.playerScores}
+            isModalVisible={modalVisible}
+            setModalVisible={setModalVisible}
+          />
+        )}
+      </Modal>
     </ImageBackground>
   );
 }
