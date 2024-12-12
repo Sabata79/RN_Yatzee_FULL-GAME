a121f7f2f9c2f4d38a09f8c1b1780a2f53539156
Sabata 79 <sabata79@hotmail.com>
2024-12-12 22:30:42 +0200

    Adjusted Header for Provider

--- components/Header.js
+++ components/Header.js
@@ -17,6 +17,7 @@ export default function Header({ isUserRecognized, name, playerId }) {
       <Text style={styles.headerTitle}>
         Yatzy <FontAwesome5 name="dice" size={35} color='#ccc9c9' />
       </Text>
+
       {isUserRecognized && name && (
         <Pressable
           style={({ pressed }) => [
@@ -24,24 +25,18 @@ export default function Header({ isUserRecognized, name, playerId }) {
             pressed && styles.userButtonPressed,
             { marginLeft: 'auto', top: -5 },
           ]}
-          onPress={() => {
-            setModalVisible(true);
-          }} 
+          onPress={() => setModalVisible(true)} 
         >
           <Text style={styles.userName}>{name}</Text>
-          <FontAwesome5
-            name="user"
-            size={22}
-            color="black"
-            style={{ marginLeft: 5 }}
-          />
+          <FontAwesome5 name="user" size={22} color="black" style={{ marginLeft: 5 }} />
         </Pressable>
       )}
 
+      {/* PlayerCard modal */}
       {isModalVisible && selectedPlayer && (
         <PlayerCard
-          playerId={selectedPlayer.playerId}
-          playerName={selectedPlayer.playerName}
+          playerId={selectedPlayer.playerId}  
+          playerName={selectedPlayer.playerName}  
           isModalVisible={isModalVisible} 
           setModalVisible={setModalVisible} 
         />
