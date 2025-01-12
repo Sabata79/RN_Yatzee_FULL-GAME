34c5f81ca670736aa893ca85632cb082eb411b1c
Sabata 79 <sabata79@hotmail.com>
2025-01-12 14:54:22 +0200

    added some opacity effect for rolling dices

--- components/DiceAnimation.js
+++ components/DiceAnimation.js
@@ -1,32 +1,32 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Animated, Pressable } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function DiceAnimation({ diceName, isSelected, onSelect, animationValue, color, isRolling }) {
+export default function DiceAnimation({ diceName, isSelected, onSelect, animationValue, color, isRolling, fadeOpacity }) {
 
-  const rotation = animationValue.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0deg', '360deg'],
-  });
+    const rotation = animationValue.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '360deg'],
+    });
 
-  const rotateY = animationValue.interpolate({
-    inputRange: [0, 0.5, 1],
-    outputRange: ['0deg', '180deg', '0deg'],
-  });
+    const rotateY = animationValue.interpolate({
+        inputRange: [0, 0.5, 1],
+        outputRange: ['0deg', '180deg', '0deg'],
+    });
 
-  const animatedStyle = {
-    transform: [
-      { rotate: rotation },
-      { rotateY: rotateY },
-    ],
-    opacity: isRolling ? 0.5 : 1,
-  }
+    const animatedStyle = {
+        transform: [
+            { rotate: rotation },
+            { rotateY: rotateY },
+        ],
+        opacity: fadeOpacity || (isRolling ? 0.5 : 1),
+    };
 
-  return (
-    <Pressable onPress={onSelect}>
-      <Animated.View style={animatedStyle}>
-        <MaterialCommunityIcons name={diceName} size={55} color={color} />
-      </Animated.View>
-    </Pressable>
-  );
+    return (
+        <Pressable onPress={onSelect}>
+            <Animated.View style={animatedStyle}>
+                <MaterialCommunityIcons name={diceName} size={55} color={color} />
+            </Animated.View>
+        </Pressable>
+    );
 }
