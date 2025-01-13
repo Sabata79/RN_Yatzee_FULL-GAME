05da37ff1a3e8433ab0365801da34584dbf85b93
Sabata 79 <sabata79@hotmail.com>
2025-01-13 12:15:02 +0200

    changes for navigation whit new landingPage

--- App.js
+++ App.js
@@ -2,29 +2,32 @@ import * as Updates from 'expo-updates';
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, Modal, View, Text, Pressable, Image } from 'react-native';
-import Scoreboard from './components/Scoreboard';
-import Gameboard from './components/Gameboard';
-import About from './components/AboutMe';
-import Rules from './components/Rules';
-import Home from './components/Home';
-import Header from './components/Header';
-import styles from './styles/styles';
-import updateModalStyles from './styles/updateModalStyles';
 import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { useFonts } from 'expo-font';
 import { GameProvider } from './components/GameContext';
-import { updateMessage } from './constants/updateMessage';
+import LandingPage from './components/LandingPage';
+import Home from './components/Home';
+import Gameboard from './components/Gameboard';
+import Scoreboard from './components/Scoreboard';
+import About from './components/AboutMe';
+import Rules from './components/Rules';
+import Header from './components/Header';
+import styles from './styles/styles';
 import { navigationImages } from './constants/NavigationImagePaths';
+import { updateMessage } from './constants/updateMessage';
+import updateModalStyles from './styles/updateModalStyles';
 
 export default function App() {
   const [isUserRecognized, setIsUserRecognized] = useState(false);
   const [name, setName] = useState('');
   const [playerId, setPlayerId] = useState('');
   const [updateAvailable, setUpdateAvailable] = useState(false);
-  const [updateModalVisible, setUpdateModalVisible] = useState(false);
+  const [updateModalVisible, setUpdateModalVisible] = useState(true);
 
+  const Stack = createStackNavigator();
   const Tab = createMaterialTopTabNavigator();
 
   useEffect(() => {
@@ -45,6 +48,11 @@ export default function App() {
     }
   }, []);
 
+  const handleUpdate = async () => {
+    setUpdateModalVisible(false);
+    await Updates.reloadAsync();
+  };
+
   const [loaded] = useFonts({
     AntonRegular: require('./assets/fonts/Anton-Regular.ttf'),
   });
@@ -53,10 +61,103 @@ export default function App() {
     return null;
   }
 
-  const handleUpdate = async () => {
-    setUpdateModalVisible(false);
-    await Updates.reloadAsync();
-  };
+  // Tab navigator for the main application screens
+  const TabNavigator = () => (
+    <Tab.Navigator
+      tabBarPosition="bottom"
+      screenOptions={({ route }) => ({
+        headerShown: false,
+        tabBarStyle: {
+          height: 80,
+          backgroundColor: 'black',
+        },
+        tabBarIcon: ({ focused }) => {
+          if (route.name === 'Home') {
+            return (
+              <Image
+                source={navigationImages[0].display}
+                style={{
+                  width: 55,
+                  height: 55,
+                  top: -12,
+                  left: -10,
+                }}
+              />
+            );
+          } else if (route.name === 'Gameboard') {
+            return (
+              <Image
+                source={navigationImages[1].display}
+                style={{
+                  width: 40,
+                  height: 40,
+                  top: -5,
+                  left: -8,
+                  opacity: focused ? 1 : 0.5,
+                }}
+              />
+            );
+          } else if (route.name === 'Scoreboard') {
+            return (
+              <Image
+                source={navigationImages[2].display}
+                style={{
+                  width: 40,
+                  height: 40,
+                  top: -5,
+                  left: -10,
+                  opacity: focused ? 1 : 0.5,
+                }}
+              />
+            );
+          } else if (route.name === 'Rules') {
+            return (
+              <Image
+                source={navigationImages[3].display}
+                style={{
+                  width: 40,
+                  height: 40,
+                  top: -5,
+                  left: -10,
+                  opacity: focused ? 1 : 0.5,
+                }}
+              />
+            );
+          } else if (route.name === 'About Me') {
+            return (
+              <Image
+                source={navigationImages[4].display}
+                style={{
+                  width: 45,
+                  height: 45,
+                  top: -5,
+                  left: -10,
+                  opacity: focused ? 1 : 0.5,
+                }}
+              />
+            );
+          }
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        options={{ tabBarStyle: { display: 'none' } }}
+      >
+        {() => (
+          <Home
+            setIsUserRecognized={setIsUserRecognized}
+            setName={setName}
+            setPlayerId={setPlayerId}
+          />
+        )}
+      </Tab.Screen>
+      <Tab.Screen name="Gameboard" component={Gameboard} />
+      <Tab.Screen name="Scoreboard" component={Scoreboard} />
+      <Tab.Screen name="Rules" component={Rules} />
+      <Tab.Screen name="About Me" component={About} />
+    </Tab.Navigator>
+  );
 
   return (
     <SafeAreaProvider>
@@ -72,7 +173,7 @@ export default function App() {
             <View style={updateModalStyles.updateModalOverlay}>
               <View style={updateModalStyles.updateModalContent}>
                 <Text style={updateModalStyles.updateModalTitle}>
-                  New Update Available !
+                  New Update Available!
                 </Text>
                 <Text style={updateModalStyles.updateModalMessage}>
                   {updateMessage}
@@ -99,123 +200,10 @@ export default function App() {
 
           <Header isUserRecognized={isUserRecognized} name={name} playerId={playerId} />
           <NavigationContainer>
-            <Tab.Navigator
-              tabBarPosition="bottom"
-              screenOptions={({ route }) => ({
-                headerShown: false,
-                tabBarShowLabel: route.name !== 'Home',
-                tabBarStyle: {
-                  height: 80,
-                  backgroundColor: 'black',
-                  borderTopWidth: 0,
-                  elevation: 0,
-                  shadowOpacity: 0,
-                },
-                tabBarActiveTintColor: '#ffffff',
-                tabBarInactiveTintColor: '#785835',
-                tabBarLabelStyle: {
-                  marginTop: 15,
-                  fontSize: 9,
-                  fontFamily: 'AntonRegular',
-                },
-                tabBarContentContainerStyle: {
-                  paddingBottom: 0,
-                  marginBottom: 0,
-                },
-                tabBarIndicatorStyle: { height: 0 ,backgroundColor: 'transparent',},
-                tabBarIcon: ({ focused }) => {
-                  if (route.name === 'Home') {
-                    return (
-                      <Image
-                        source={navigationImages[0].display}
-                        style={{
-                          width: 55,
-                          height: 55,
-                          top: -12,
-                          left: -10,
-                        }}
-                      />
-                    );
-                  } else if (route.name === 'Gameboard') {
-                    return (
-                      <Image
-                        source={navigationImages[1].display}
-                        style={{
-                          width: 40,
-                          height: 40,
-                          top: -5,
-                          left: -8,
-                          opacity: focused ? 1 : 0.5,
-                        }}
-                      />
-                    );
-                  } else if (route.name === 'Scoreboard') {
-                    return (
-                      <Image
-                        source={navigationImages[2].display}
-                        style={{
-                          width: 40,
-                          height: 40,
-                          top: -5,
-                          left: -10,
-                          opacity: focused ? 1 : 0.5,
-                        }}
-                      />
-                    );
-                  } else if (route.name === 'Rules') {
-                    return (
-                      <Image
-                        source={navigationImages[3].display}
-                        style={{
-                          width: 40,
-                          height: 40,
-                          top: -5,
-                          left: -10,
-                          opacity: focused ? 1 : 0.5,
-                        }}
-                      />
-                    );
-                  } else if (route.name === 'About Me') {
-                    return (
-                      <Image
-                        source={navigationImages[4].display}
-                        style={{
-                          width: 45,
-                          height: 45,
-                          top: -5,
-                          left: -10,
-                          opacity: focused ? 1 : 0.5,
-                        }}
-                      />
-                    );
-                  }
-                },
-                swipeEnabled: isUserRecognized,
-              })}
-            >
-              <Tab.Screen
-                name="Home"
-                options={{
-                  tabBarStyle: { display: 'none' },
-                }}
-              >
-                {() => (
-                  <Home
-                    setIsUserRecognized={setIsUserRecognized}
-                    setName={setName}
-                    setPlayerId={setPlayerId}
-                  />
-                )}
-              </Tab.Screen>
-              <Tab.Screen
-                name="Gameboard"
-                component={Gameboard}
-                playerId={playerId}
-              />
-              <Tab.Screen name="Scoreboard" component={Scoreboard} />
-              <Tab.Screen name="Rules" component={Rules} />
-              <Tab.Screen name="About Me" component={About} />
-            </Tab.Navigator>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+              <Stack.Screen name="LandingPage" component={LandingPage} />
+              <Stack.Screen name="MainApp" component={TabNavigator} />
+            </Stack.Navigator>
           </NavigationContainer>
           <StatusBar style="light" backgroundColor="black" />
         </SafeAreaView>
--- components/Home.js
+++ components/Home.js
@@ -1,217 +1,83 @@
-import React, { useState, useEffect, useRef } from "react";
-import { View, Text, TextInput, Pressable, Alert, ImageBackground, ActivityIndicator, Image, Animated } from "react-native";
-import { MaterialCommunityIcons } from '@expo/vector-icons';
-import { FontAwesome5 } from '@expo/vector-icons';
-import styles from '../styles/styles';
-import * as SecureStore from 'expo-secure-store';
-import { database } from '../components/Firebase';
-import { ref, onValue, set, get } from 'firebase/database';
-import uuid from 'react-native-uuid';
-import { useNavigation } from '@react-navigation/native';  
-import { useGame } from '../components/GameContext';
+import React from "react";
+import { View, Text, Pressable, ImageBackground, Image } from "react-native";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { FontAwesome5 } from "@expo/vector-icons";
+import styles from "../styles/styles";
+import { useNavigation } from "@react-navigation/native";
+import { useGame } from "../components/GameContext";
 
-export default function Home({ setIsUserRecognized, setName, setPlayerId }) {
-  const [localName, setLocalName] = useState('');
-  const [playerId, setLocalPlayerId] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [isUserRecognized, setUserRecognized] = useState(false);
-  const inputRef = useRef(null);
+export default function Home() {
   const navigation = useNavigation();
-  const fadeAnim = useRef(new Animated.Value(0)).current; // Animaation alustus
-
-  const { setPlayerIdContext, setPlayerNameContext } = useGame();
-
-  useEffect(() => {
-    if (localName && playerId) {
-      setPlayerIdContext(playerId);
-      setPlayerNameContext(localName);
-    }
-  }, [localName, playerId]);
-
-  useEffect(() => {
-    getOrCreateUserId().then((userId) => {
-      setLocalPlayerId(userId);
-      checkExistingUser(userId);
-    });
-  }, []);
-
-  const getOrCreateUserId = async () => {
-    let userId = await SecureStore.getItemAsync('user_id');
-    if (!userId) {
-      userId = uuid.v4();
-      await SecureStore.setItemAsync('user_id', userId);
-    }
-    return userId;
-  };
-
-  const checkExistingUser = (userId) => {
-    const playerRef = ref(database, `players/${userId}`);
-    onValue(playerRef, (snapshot) => {
-      const playerData = snapshot.val();
-      if (playerData) {
-        setLocalName(playerData.name);
-        setName(playerData.name);
-        setUserRecognized(true);
-        setIsUserRecognized(true);
-      } else {
-        setUserRecognized(false);
-        setIsUserRecognized(false);
-      }
-      setLoading(false);
-    });
-  };
-
-  useEffect(() => {
-    if (!loading) {
-      Animated.timing(fadeAnim, {
-        toValue: 1, 
-        duration: 1000, 
-        useNativeDriver: true,
-      }).start();
-    }
-  }, [loading]);
-
-  const checkIfNameExists = async (name) => {
-    const playersRef = ref(database, 'players');
-    const snapshot = await get(playersRef);
-    if (snapshot.exists()) {
-      const playersData = snapshot.val();
-      for (let playerId in playersData) {
-        if (playersData[playerId].name === name) {
-          return true;
-        }
-      }
-    }
-    return false;
-  };
-
-  const saveNewPlayer = async (name, userId) => {
-    const playerRef = ref(database, `players/${userId}`);
-    const snapshot = await get(playerRef);
-    const playerData = snapshot.val();
-
-    set(playerRef, {
-      ...playerData,
-      name: name,
-      dateJoined: playerData?.dateJoined || new Date().toLocaleDateString(),
-    });
-
-    setName(name);
-    setPlayerId(userId);
-  };
-
-  const handlePress = async () => {
-    if (localName.trim() === '') {
-      Alert.alert('Name is required', 'Please enter your name.');
-    } else if (localName.length < 3 || localName.length > 10) {
-      Alert.alert('Name is too short', 'Please enter a nickname with at least 3 characters and maximum 10 characters.');
-    } else {
-      const nameExists = await checkIfNameExists(localName);
-      if (nameExists) {
-        Alert.alert('Name already in use', 'That nickname is already in use. Please choose another.');
-      } else {
-        setUserRecognized(true);
-        setIsUserRecognized(true);
-
-        if (!playerId) {
-          const newPlayerId = uuid.v4();
-          setLocalPlayerId(newPlayerId); 
-          setPlayerId(newPlayerId);
-          setPlayerIdContext(newPlayerId); 
-          setPlayerNameContext(localName); 
-          saveNewPlayer(localName, newPlayerId);
-        } else {
-          setPlayerIdContext(playerId);
-          setPlayerNameContext(localName);
-          saveNewPlayer(localName, playerId);
-        }
-      }
-    }
-  };
+  const { playerName } = useGame();
 
   const handlePlay = () => {
-    navigation.navigate('Gameboard');
+    navigation.navigate("Gameboard");
+  };
+
+  const handleRules = () => {
+    navigation.navigate("Rules");
   };
 
   const handleChangeName = () => {
-    setLocalName('');
-    setUserRecognized(false);
-    setIsUserRecognized(false);
+    navigation.navigate("LandingPage");
   };
 
   return (
-    <ImageBackground source={require('../assets/diceBackground.jpg')} style={styles.background}>
+    <ImageBackground source={require("../assets/diceBackground.jpg")} style={styles.background}>
       <View style={styles.overlay}>
-        {loading ? (
-          <View style={styles.loadingContainer}>
-            <ActivityIndicator 
-              size={'80%'}
-              color="#00ff00"
-              animating={true} />
-            <Text style={[styles.rulesText, { color: '#62a346' }]}>Checking player data...</Text>
-          </View>
-        ) : (
-          <Animated.View style={[styles.rulesContainer, { opacity: fadeAnim }]}>
-            {!isUserRecognized ? (
-              <View style={styles.rulesContainer}>
-                <Text style={styles.rulesText}>Hi, Stranger! Can you tell your nickname?</Text>
-                <Text style={styles.rulesAuxillaryText}>( Nickname must be 3-10 characters long. )</Text>
-                <Image source={require('../assets/register.png')} 
-                        style={styles.registerImage} 
-                />
-                <TextInput
-                  ref={inputRef}
-                  style={styles.input}
-                  placeholder="Enter your nickname"
-                  placeholderTextColor={'white'}
-                  value={localName}
-                  onChangeText={(val) => setLocalName(val)}
-                  autoFocus={false}
-                />
+        {playerName ? (
+          // Jos pelaaja on tunnistettu
+          <View style={styles.rulesContainer}>
+            <Text style={styles.rulesText}>Hi {playerName}, let's roll the dice!</Text>
+            <Image
+              source={require("../assets/hiThere.png")}
+              style={styles.hiThereImage}
+            />
+            <View style={styles.homeButtonContainer}>
+              <View style={styles.rowButtons}>
                 <Pressable
-                  style={({ pressed }) => [styles.homeButton, pressed && styles.homeButtonPressed]}
-                  onPress={handlePress}
+                  style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+                  onPress={handleChangeName}
                 >
-                  <Text style={styles.buttonText}>OK</Text>
+                  <Text style={[styles.buttonText, { fontSize: 16 }]}>Change name</Text>
+                </Pressable>
+                <Pressable
+                  style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
+                  onPress={handlePlay}
+                >
+                  <Text style={styles.buttonText}>PLAY</Text>
+                  <MaterialCommunityIcons name="play" size={30} color="black" />
                 </Pressable>
               </View>
-            ) : (
-              <View style={styles.rulesContainer}>
-                <Text style={styles.rulesText}>Hi {localName}, let's roll the dice!</Text>
-                <Image source={require('../assets/hiThere.png')} 
-                  style={styles.hiThereImage} 
-                />
-                <View style={styles.homeButtonContainer}>
-                  <View style={styles.rowButtons}>
-                    <Pressable
-                      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
-                      onPress={handleChangeName}
-                    >
-                      <Text style={[styles.buttonText, { fontSize: 16 }]}>Change name</Text>
-                    </Pressable>
-                    <Pressable
-                      style={({ pressed }) => [styles.button, pressed && styles.buttonPressed]}
-                      onPress={handlePlay}
-                    >
-                      <Text style={styles.buttonText}>PLAY</Text>
-                      <MaterialCommunityIcons name="play" size={30} color="black" />
-                    </Pressable>
-                  </View>
-                  <Pressable
-                    style={({ pressed }) => [
-                      styles.button,
-                      pressed && styles.buttonPressed,
-                      styles.fullWidthButton,
-                    ]}
-                    onPress={() => navigation.navigate('Rules')}
-                  >
-                    <Text style={styles.buttonText}>Rules</Text>
-                    <FontAwesome5 name="book" size={30} color="black" />
-                  </Pressable>
-                </View>
-              </View>
-            )}
-          </Animated.View>
+              <Pressable
+                style={({ pressed }) => [
+                  styles.button,
+                  pressed && styles.buttonPressed,
+                  styles.fullWidthButton,
+                ]}
+                onPress={handleRules}
+              >
+                <Text style={styles.buttonText}>Rules</Text>
+                <FontAwesome5 name="book" size={30} color="black" />
+              </Pressable>
+            </View>
+          </View>
+        ) : (
+          // Jos pelaajaa ei ole tunnistettu
+          <View style={styles.rulesContainer}>
+            <Text style={styles.rulesText}>Hi, Stranger! Can you tell your nickname?</Text>
+            <Text style={styles.rulesAuxillaryText}>( Nickname must be 3-10 characters long. )</Text>
+            <Image
+              source={require("../assets/register.png")}
+              style={styles.registerImage}
+            />
+            <Pressable
+              style={({ pressed }) => [styles.homeButton, pressed && styles.homeButtonPressed]}
+              onPress={() => navigation.navigate("LandingPage")}
+            >
+              <Text style={styles.buttonText}>OK</Text>
+            </Pressable>
+          </View>
         )}
       </View>
     </ImageBackground>
